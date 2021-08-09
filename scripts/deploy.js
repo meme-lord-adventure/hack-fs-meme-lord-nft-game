dd9039a5c7fdc3b6a130d76e75564da63c021d11
Latricia <tricianickelberry@gmail.com>
2021-08-09 14:00:18 -0700

    added hardhat/meme NFT contract

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    // Grab the contract factory 
+    const MemeLord = await ethers.getContractFactory("MemeLord");
+    
+    // Start deployment, returning a promise that resolves to a contract object
+    const memeLord = await MemeLord.deploy(); // Instance of the contract 
+    console.log("Contract deployed to address:", memeLord.address);
+ }
+ 
+ main()
+   .then(() => process.exit(0))
+   .catch(error => {
+     console.error(error);
+     process.exit(1);
+   });
